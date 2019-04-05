3221e871ab3047c83c24db69b459e293f4f0e11c
Max Lee <maxwell.a.lee56@gmail.com>
2019-04-04 21:53:42 -0400

    Added an order button, made name a sticky field, and made errors checking if fields are empty or not

--- index.php
+++ index.php
@@ -15,24 +15,74 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Your name:
-                        <input type="text" class="form-control" name="name">
-                    </label>
-                </div>
-                <p>Cupcake flavors</p>
-                <?php
-                $flavors = array("grasshopper"=>"The Grasshopper","maple"=>"Whiskey Maple Bacon",
-                    "carrot"=>"Carrot Walnut","caramel"=>"Salted Caramel Cupcake","velvet"=>"Red Velvet",
-                    "lemon"=>"Lemon Drop","tiramisu"=>"Tiramisu");
-                foreach ($flavors as $flavor => $name)
-                {
-                    echo '<div class="form-check">
-                        <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" name="'.$flavor.'">
-                            '.$name.'
+                <form method="post">
+                    <div class="form-group">
+                        <label>Your name:
+                            <input type="text" class="form-control" name="name" value="<?php
+                            if(isset($_POST['name'])) echo $_POST['name'];?>">
                         </label>
-                    </div>';
+                    </div>
+                    <p>Cupcake flavors:</p>
+                    <?php
+                    $flavors = array("grasshopper"=>"The Grasshopper","maple"=>"Whiskey Maple Bacon",
+                        "carrot"=>"Carrot Walnut","caramel"=>"Salted Caramel Cupcake","velvet"=>"Red Velvet",
+                        "lemon"=>"Lemon Drop","tiramisu"=>"Tiramisu");
+                    foreach ($flavors as $flavor => $name)
+                    {
+                        echo '<div class="form-check">'.
+                            '<label class="form-check-label">'.
+                                '<input class="form-check-input" type="checkbox" name="'.$flavor.'">'.
+                                $name.
+                            '</label>'.
+                        '</div>';
+                    }
+                    ?>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Order</button>
+                </form>
+                <?php
+                if($_SERVER['REQUEST_METHOD'] == 'POST')
+                {
+                    $errors = [];
+
+                    if(!empty($_POST['name']))
+                    {
+                        $name = $_POST['name'];
+                    }
+                    else
+                    {
+                        $errors[] = "You forgot to add your name.";
+                    }
+
+                    $flavors_picked = [];
+                    foreach ($flavors as $flavor)
+                    {
+                        if(!empty($_POST['"'.$flavor.'"']))
+                        {
+                            $flavors_picked[] = $flavor;
+                        }
+                    }
+
+                    if (empty($flavors_picked))
+                    {
+                        $errors[] = "You forgot to pick any cupcakes.";
+                    }
+
+                    if(empty($errors))
+                    {
+
+                    }
+                    else
+                    {
+                        echo '<div class="alert alert-danger" role="alert"><h4>Error!</h4>';
+
+                        echo '<p class="error">The following error(s) occurred:<br>';
+                        foreach ($errors as $msg) { // Print each error.
+                            echo " - $msg<br>\n";
+                        }
+                        echo '</p><p>Please try again.</p><p><br></p>';
+                        echo '</div>';
+                    }
                 }
                 ?>
             </div>
